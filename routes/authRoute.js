21f3f1d2a2d4034129bbf408643131c2c08c1e09
varun123936 <tejavarun689@gmail.com>
2023-02-12 13:34:04 +0530

    added isAdmin middleware function

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { createUser, loginUserCtrl,getallUser, getaUser,deleteaUser,updatedUser } = require("../controller/userCtrl");
-const { authMiddleware } = require("../middlewares/authMiddleware");
+const { authMiddleware,isAdmin } = require("../middlewares/authMiddleware");
 const router=express.Router()
 
 router.post('/register',createUser);
 router.post('/login',loginUserCtrl);
 router.get('/all-users',getallUser);
-router.get('/:id',authMiddleware,getaUser);
+router.get('/:id',authMiddleware,isAdmin,getaUser);
 router.delete('/:id',deleteaUser)
 router.put('/:id',updatedUser)
 
